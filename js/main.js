24c6a9ed0a5a000a9ade1592e9c400c17bfec49e
lkhan69420 <b2022lskhan@pshs.edu.ph>
2020-07-03 01:46:17 +0800

    added parts 2, 3, 4, 5

--- js/main.js
+++ js/main.js
@@ -2,12 +2,55 @@ $(document).ready(function() {
   $(".c1").click(function() {
       $(".o1").css("display", "block");
       $(".remove").css("display", "block");
-      // $(".i1").css("display", "block");
+      // $(".sections").css("display", "none");
       $(".o1").animate({
           width: "100%",
       }, 1000);
       setTimeout(function() {
-          $(".title").css("display", "block");
+          $(".o1 .title").css("display", "block");
+      }, 1200);
+  });
+  $(".c2").click(function() {
+      $(".o2").css("display", "block");
+      $(".remove").css("display", "block");
+      // $(".sections").css("display", "none");
+      $(".o2").animate({
+          width: "100%",
+      }, 1000);
+      setTimeout(function() {
+          $(".o2 .title").css("display", "block");
+      }, 1200);
+  });
+  $(".c3").click(function() {
+      $(".o3").css("display", "block");
+      $(".remove").css("display", "block");
+      // $(".sections").css("display", "none");
+      $(".o3").animate({
+          width: "100%",
+      }, 1000);
+      setTimeout(function() {
+          $(".o3 .title").css("display", "block");
+      }, 1200);
+  });
+  $(".c4").click(function() {
+      $(".o4").css("display", "block");
+      $(".remove").css("display", "block");
+      // $(".sections .back").css("display", "none");
+      $(".o4").animate({
+          width: "100%",
+      }, 1000);
+      setTimeout(function() {
+          $(".o4 .title").css("display", "block");
+      }, 1200);
+  });
+  $(".c5").click(function() {
+      $(".o5").css("display", "block");
+      $(".remove").css("display", "block");
+      $(".o5").animate({
+          width: "100%",
+      }, 1000);
+      setTimeout(function() {
+          $(".o5 .title").css("display", "block");
       }, 1200);
   });
   $(".remove").click(function() {
